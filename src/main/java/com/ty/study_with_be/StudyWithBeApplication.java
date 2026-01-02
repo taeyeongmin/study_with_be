01d90e5c94949ab0a8b2d7fe9b35bf496f6a60e4
taeyeong <alsxodud123@naver.com>
2026-01-02 16:24:32 +0900

    first commit

---
+++ src/main/java/com/ty/study_with_be/StudyWithBeApplication.java
@@ -0,0 +1,13 @@
+package com.ty.study_with_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyWithBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyWithBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ty/study_with_be/StudyWithBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ty.study_with_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyWithBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
